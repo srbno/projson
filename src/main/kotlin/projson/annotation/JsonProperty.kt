a5eea180bf2ccf1720ea856379bfb85b4cad9483
Silvio Romano <srbno@iscte-iul.pt>
2026-05-17 02:49:23 +0100

    Documenta validação de nomes em @JsonProperty

--- src/main/kotlin/projson/annotation/JsonProperty.kt
+++ src/main/kotlin/projson/annotation/JsonProperty.kt
@@ -6,6 +6,9 @@ package projson.annotation
  * This annotation lets a Kotlin property keep its original name in code while being rendered with
  * a different property identifier in the generated JSON object.
  *
+ * Serialization throws `IllegalArgumentException` when [value] is blank or contains only whitespace.
+ * The names `$id`, `$ref`, `$type`, `id` and `type` are reserved and will also be rejected.
+ *
  * @property value name to use in the JSON output.
  */
 @Retention(AnnotationRetention.RUNTIME)
